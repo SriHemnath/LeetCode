340b0f4c8a5dad14357a65eaf4989dd3e9376586
hjaishan <hemnath.j1995@gmail.com>
2023-10-04 15:53:09 +0530

    two sum

---
+++ TwoSum/go.mod
@@ -0,0 +1,3 @@
+module github.com/SriHemnath/LeetCode/TwoSum
+
+go 1.20
---
+++ TwoSum/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	fmt.Println(result)
+}
+
+func twoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
+
+	for i, num := range nums {
+		complement := target - num
+		if idx, ok := numMap[complement]; ok {
+			return []int{idx, i}
+		}
+		numMap[num] = i
+	}
+
+	return nil // No solution found
+}
